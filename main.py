e2a6b0d2daec91f0ef101e090d41c9a6c920e601
eclipse902 <68570590+ThomasDeOliv@users.noreply.github.com>
2025-01-24 23:07:07 +0100

    Decouplage de l'architecture

--- main.py
+++ main.py
@@ -1,69 +1,42 @@
 import pygame
 import time
 import random
+import pygame.locals
 from utils.color import Colors
-from pygame.locals import *
+from utils.game_over import GameOver
 from utils.generaluse import GeneralUse
-
-
-WIDTH, HEIGHT = 1600, 1000
+from utils.window import HEIGHT, WIDTH
+from pygame.font import SysFont
 
 pygame.init()
 pygame.display.set_caption("The rise of the Axolotl")
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
-
-#############
-
-### Font
-
-font = pygame.font.SysFont("serif", 40)
-
-print(Colors.DARK_GRAY)
+font = SysFont(name="serif", size=40)
 
 #############
 general_use = GeneralUse(screen)
 
-class game_over:
-
-    def draw_window(self):
-        general_use.display_background()
-        
-        score_text = font.render(f"Score : {main.score}", 1, Colors.BLACK)
-        screen.blit(score_text, (WIDTH//2 - score_text.get_width()//2, HEIGHT//2 - score_text.get_height()//2))
-
-        pygame.display.update()
-
-    def game_over(self):
-        run = True
-        while run:
-            clock.tick(60)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    general_use.close_the_game()
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        run = False
-                        general_use.close_the_game()
-            self.draw_window()
-
-game_over = game_over()
-
-
-
 class main_game:
-    player = pygame.Rect(WIDTH//2, 10, 50, 50)
-    floor = pygame.Rect(0, HEIGHT - 50, WIDTH, 10)
-    velocity = 0
-    falling_speed = 3
-    speed = 25
-    item = None
-    score = 0
-    start_time = time.time()
-    game_length = 15
+
+    def __init__(self):
+        self.player = pygame.Rect(WIDTH//2, 10, 50, 50)
+        self.floor = pygame.Rect(0, HEIGHT - 50, WIDTH, 10)
+        self.velocity = 0
+        self.falling_speed = 3
+        self.speed = 25
+        self.item = None
+        self.score = 0
+        self.start_time = time.time()
+        self.game_length = 15
+        self.get_score_text = lambda: font.render(f"Score : {self.score}", True, Colors.BLACK)
+        self.game_over = GameOver(
+            general_use,
+            clock,
+            self.get_score_text,
+            screen,
+        )
 
     def get_random_item_x(self):
         x = random.random() * WIDTH
@@ -76,6 +49,7 @@ class main_game:
 
     def spawn_item(self):
         run = True
+        x: float = 0
         if self.item is None:
             x = self.get_random_item_x()
         else:
@@ -101,10 +75,9 @@ class main_game:
         
         pygame.draw.rect(screen, Colors.BLACK, self.floor)
 
-        score_text = font.render(f"Score : {self.score}", 1, Colors.BLACK)
-        screen.blit(score_text, (10, 10))
+        screen.blit(self.get_score_text(), (10, 10))
 
-        time_text = font.render(f"{self.game_length - (time.time() - self.start_time):.2f} s", 1, Colors.BLACK)
+        time_text = font.render(f"{self.game_length - (time.time() - self.start_time):.2f} s", True, Colors.BLACK)
         screen.blit(time_text, (WIDTH - time_text.get_width() - 10, 10))
 
         pygame.display.update()
@@ -135,24 +108,24 @@ class main_game:
             self.pickup_item()
 
             if (self.game_length - (time.time() - main.start_time)) <= 0:
-                game_over.game_over()
+                self.game_over.game_over()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                     general_use.close_the_game()
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
                         run = False
                         general_use.close_the_game()
-                    if event.key == K_q:
+                    if event.key == pygame.K_q:
                         left = True
-                    if event.key == K_d:
+                    if event.key == pygame.K_d:
                         right = True
-                if event.type == KEYUP:
-                    if event.key == K_q:
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_q:
                         left = False
-                    if event.key == K_d:
+                    if event.key == pygame.K_d:
                         right = False
                     
             self.draw_window()

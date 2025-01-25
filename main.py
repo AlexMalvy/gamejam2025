39e92b69b9c7861f220dd807ed1013be8c58a52e
Alex MALVY <142165167+AlexMalvy@users.noreply.github.com>
2025-01-25 02:48:51 +0100

    Added a camera/map

--- main.py
+++ main.py
@@ -5,35 +5,35 @@ from utils.color import Colors
 from utils.game_over import GameOver
 from character import Character
 from utils.generaluse import GeneralUse
-from utils.camera import Camera
+from utils.map import Map
 from utils.window import HEIGHT, WIDTH
 from pygame.font import SysFont
 from game_menu import GameMenu
 from pygame.locals import *
 
 
-WIDTH, HEIGHT = 1600, 1000
+# WIDTH, HEIGHT = 1600, 1000
 
 pygame.init()
 pygame.display.set_caption("The rise of the Axolotl")
-screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
-background = pygame.image.load("assets/background/gamejam-2025-axolotl-fond-provisoire-1920x12959.jpg")
-map = pygame.surface.Surface((background.get_width(), background.get_height()))
+# screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+WIDTH, HEIGHT = screen.get_width(), screen.get_height()
 clock = pygame.time.Clock()
 font40 = SysFont(name="serif", size=40)
 font50 = SysFont(name="serif", size=50)
 
 class MainGame:
     def __init__(self):
-        self.player = Character(2, 10, (WIDTH//2, 0), "assets/player/player_idle.png")
+        self.player = Character(3, 10, (WIDTH//2, 0), "assets/player/player_idle.png")
         self.player_group = pygame.sprite.Group()
         self.player_group.add(self.player)
-        self.floor = pygame.Rect(0, HEIGHT - 50, WIDTH, 10)
         self.velocity = 0
         self.falling_speed = 3
         self.speed = 25
         self.start_time = time.time()
         self.game_length = 15
+
         self.general_use = GeneralUse(screen)
         self.game_over = GameOver(
             self.general_use,
@@ -42,27 +42,31 @@ class MainGame:
             screen,
         )
         self.game_menu = GameMenu(screen, font50)
+
+        self.map = Map(self.player, screen)
+
+        self.floor = pygame.Rect(0, self.map.map.height - 50, WIDTH, 10)
     
 
     def draw_window(self):
-        self.general_use.display_background()
+        self.map.draw_bg()
         
-        pygame.draw.rect(screen, Colors.BLACK, self.floor)
+        pygame.draw.rect(self.map.map, Colors.BLACK, self.floor)
 
-        self.player_group.draw(screen)
+        self.player_group.draw(self.map.map)
         self.player_group.update()
+
+        self.map.update()
 
         pygame.display.update()
 
     def game_loop(self):
-        camera = Camera()
         run = True
         left = False
         right = False
         up = False
         while run:
             clock.tick(60)
-            mouse_pos = pygame.mouse.get_pos()
 
             # Movements
             if left and self.player.rect.left > 0:
@@ -91,11 +95,11 @@ class MainGame:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-                    GeneralUse.close_the_game()
+                    self.general_use.close_the_game()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         run = False
-                        GeneralUse.close_the_game()
+                        self.general_use.close_the_game()
                     if event.key == pygame.K_q:
                         left = True
                     if event.key == pygame.K_d:
@@ -110,7 +114,7 @@ class MainGame:
             self.draw_window()
 
     def run(self):
-        self.game_menu.menu_loop()
+        # self.game_menu.menu_loop()
         self.game_loop()
 
 main = MainGame()

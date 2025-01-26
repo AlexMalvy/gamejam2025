159ae2b6b03285131a89067cf39177d850bbf222
Alex MALVY <142165167+AlexMalvy@users.noreply.github.com>
2025-01-26 13:21:45 +0100

    reduced cat alpha added y movement to cat changed y position of the cat

--- src/entities/cat.py
+++ src/entities/cat.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from src.entities.character import Character
 from src.utils.map import Map
+import math
 
 
 class Cat(Character):
@@ -15,15 +16,20 @@ class Cat(Character):
         super().__init__(sheets_path, pos, scale, animation_speed)
         self.map = map
         self.animation_speed = 20
+        self.base_y = self.rect.centery
 
     def update(self):
         super().update()
+
+        self.image.set_alpha(100)
 
         if not self.rect.colliderect(self.map.map_rect):
             self.turn_around()
 
         # Move
         self.rect.x += self.speed
+
+        self.rect.y = self.base_y + 50 * math.sin(self.rect.x / 50)
 
     def turn_around(self):
         self.speed = -self.speed

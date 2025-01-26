73a3cc1399f8136dba646a325532cf889b20e9d1
eclipse902 <68570590+ThomasDeOliv@users.noreply.github.com>
2025-01-26 16:14:54 +0100

    fixed_fullscreen by adding base resolution and SCALED flag

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ from background_entities import BackgroundEntities
 from src.utils.sounds import SoundManager
 
 
-WIDTH, HEIGHT = 1600, 800
+#WIDTH, HEIGHT = 1600, 800
 
 pygame.init()
 pygame.display.set_caption("The rise of the Axolotl")
-screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
-# screen = pygame.display.set_mode((0,0)) #pygame.FULLSCREEN
-# WIDTH, HEIGHT = screen.get_width(), screen.get_height()
+#screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+screen = pygame.display.set_mode(size=(1600,800), flags=pygame.FULLSCREEN | pygame.SCALED)
+WIDTH, HEIGHT = screen.get_width(), screen.get_height()
 clock = pygame.time.Clock()
 
 font_path = "assets/fonts/nexa_heavy.ttf"
